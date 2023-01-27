6c1a0145d063ba962e5ae397e0f19692d5e46a0f
June <76894305+parkkhee@users.noreply.github.com>
2023-01-27 14:24:17 +0900

    G5 / 00:10:09 / X

---
+++ parkkhee/1038.java
@@ -0,0 +1,32 @@
+
+import java.io.*;
+import java.nio.Buffer;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {  //1038번 한번 보여주셔야겠습니다,,
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int n;
+
+
+    public static void main(String[] args) throws IOException {
+
+        n = Integer.parseInt(br.readLine());
+
+
+
+    }
+
+    public static void f(int num) {
+
+        if(num)
+
+    }
+
+
+
+}
+
+
+
