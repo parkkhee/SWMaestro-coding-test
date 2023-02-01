c1de2fb8573de9b68bb652f77a5ac931f380c836
June <76894305+parkkhee@users.noreply.github.com>
2023-02-01 22:12:36 +0900

    S3 / 00:10:07 / X

---
+++ parkkhee/7795.java
@@ -0,0 +1,33 @@
+
+import java.io.*;
+import java.nio.Buffer;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int testcase;
+
+
+    public static void main(String[] args) throws IOException {
+
+        testcase = Integer.parseInt(br.readLine());
+
+        while (<testcase) {
+
+
+
+        }
+
+
+    }
+
+
+
+
+}
+
+
+
