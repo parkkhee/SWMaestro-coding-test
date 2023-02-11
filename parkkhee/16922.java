f04de897f720f28046038ffd505763302c72b5e1
June <76894305+parkkhee@users.noreply.github.com>
2023-02-11 22:50:42 +0900

    S3 / 00:10:22 / X

---
+++ parkkhee/16922.java
@@ -0,0 +1,38 @@
+package test;
+import java.io.*;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int n;
+    static int cnt = 0;
+    static boolean[] visit;
+
+    public static void main(String[] args) throws IOException {
+
+        n = Integer.parseInt(br.readLine());
+
+
+        visit = new boolean[50 * 20 + 1];
+        for (int i = 0; i < 4; i++) {
+
+        }
+
+
+    }
+
+    public void f(int start) {
+
+        for (int i = 0; i < 4; i++) {
+
+        }
+
+    }
+
+
+
+
+}
+
